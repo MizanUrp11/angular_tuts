7c432383c26d00c101c0d8ed5cc5f177e54a14c3
MizanUrp11 <mrn1105009@gmail.com>
2020-07-09 08:35:04 +0600

    From input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from "@angular/router";
 import { MatSliderModule } from "@angular/material/slider"
 import { MatButtonModule } from "@angular/material/button";
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { HeroesListComponent } from './heroes-list/heroes-list.component';
+import { NameEditorComponent } from './name-editor/name-editor.component';
+import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CrisisListComponent,
     HeroesListComponent,
+    NameEditorComponent,
+    ProfileEditorComponent,
   ],
   imports: [
     BrowserModule,
@@ -22,6 +27,7 @@ import { HeroesListComponent } from './heroes-list/heroes-list.component';
     BrowserAnimationsModule,
     MatSliderModule,
     MatButtonModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
       {path: 'crisis-list', component: CrisisListComponent},
       {path: 'heroes-list',component: HeroesListComponent},
