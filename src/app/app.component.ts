3d9cf677bf60e07a3ad1a58e0aa865d6ff86284b
MizanUrp11 <mrn1105009@gmail.com>
2020-07-09 19:43:57 +0600

    End of Attribute directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { FormControl } from "@angular/forms";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  color = ''; //if not set will effective from highlight.directive.ts
   title = 'fundamentals';
   myControl = new FormControl();
   options: string[] = ['one','two','three'];
